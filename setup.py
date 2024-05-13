f213afb45c5fb155be6a20980baf378d9f6cbd0b
Tiwari13579 <siddhanathtiwari7709@gmail.com>
2024-05-13 11:15:25 +0530

    data added

--- setup.py
+++ setup.py
@@ -1,29 +1,30 @@
 import setuptools
 
-# with open("README.md", "r", encoding="utf-8") as f:
-#     long_description = f.read()
+try:
+    with open("README.md", "r", encoding="utf-8") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = "No README file found."
 
-
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 REPO_NAME = "End-to-end-ML-Project-Implementation"
 AUTHOR_USER_NAME = "siddhanth"
 SRC_REPO = "mlProject"
 AUTHOR_EMAIL = "siddhanath7709@gmail.com"
 
-
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A small python package for ml app",
-    #long_description=long_description,
-    long_description_content="text/markdown",
+    description="A small python package for an ML app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
+    packages=setuptools.find_packages(where="src"),
 )
